b80fadc95f4a0cb95bfc6174f6237ff25e280956
Matt Pierson <matt.pierson789@gmail.com>
2023-03-20 17:01:40 -0400

    my_join

--- enumerables.rb
+++ enumerables.rb
@@ -169,4 +169,77 @@ end
     d = [13, 14, 15]
     p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
+
+# My Rotate
+# Write a method my_rotate that returns a new array containing 
+# all the elements of the original array in a rotated order.
+#  By default, the array should rotate by one element. 
+# If a negative value is given, the array is rotated in the opposite direction.
+
+# Example:
+
+def my_rotate(num = 1)
+    
+    new_array = []
+
+    i = 0 
+
+    while i < self.length 
+
+        new_array << self[(i + num) % self.length] 
+
+        i+= 1 
+    end 
+
+
+    new_array
+
+end 
+
+
+
+
+p a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]  
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+
+
+# My Join
+# my_join returns a single string containing all the elements of the array,
+#  separated by the given string separator. If no separator is given, an empty string is used.
+
+# Example:
+
+def my_join(seperator = "")
+
+    new_str = ""
+
+    self.my_each do |ele|
+        new_str += ele + seperator
+    end 
+
+    
+    if seperator != ""
+        new_str[0...-1] 
+    else  
+        new_str 
+    end 
+
+
+end 
+
+p a = [ "a", "b", "c", "d" ]
+p a.my_join         # => "abcd"
+p a.my_join("$")    # => "a$b$c$d"
+
+
+
+
+
+
+
+
+
 end 

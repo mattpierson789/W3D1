9f6fc93145974549e30e4ae8b53ce84aa5dfe1ec
carveyh <carvey.hor@gmail.com>
2023-03-20 16:37:20 -0400

    my_zip

--- enumerables.rb
+++ enumerables.rb
@@ -138,6 +138,35 @@ end
     p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
     p [1,[2,3]].my_flatten
 
+    # my_zip
+    # Write my_zip to take any number of arguments. It should return a new array
+    # containing self.length elements. Each element of the new array should be an 
+    # array with a length of the input arguments + 1 and contain the merged elements at that index. 
+    # If the size of any argument is less than self, nil is returned for that location
 
+    def my_zip(*arrays)
+        new_array = []
+        (self.length).times do |i|
+            arr = [self[i]]
+            arrays.each do |array|
+                arr << array[i]
+            end
+            new_array << arr
+        end
+        new_array
+    end
+
+    puts
+    puts "my_zip"
+    a = [ 4, 5, 6 ]
+    b = [ 7, 8, 9 ]
+    p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+    
+    p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+    p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+    c = [10, 11, 12]
+    d = [13, 14, 15]
+    p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
 
 end 
